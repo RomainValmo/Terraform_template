21b718fec8389c86b22a776f0db3547d7803b1c6
RomainValmo <142975520+RomainValmo@users.noreply.github.com>
2024-08-15 14:05:34 +0200

    Fetch Gcp credentials in Vault

---
+++ Credentials_GCP_in_Vault/main.tf
@@ -0,0 +1,38 @@
+provider "google" {
+    credentials = jsonencode(data.vault_generic_secret.gcp_owner.data)
+    project     = var.project_id
+}
+
+provider "vault" {
+   address   = var.vault.vault_address
+  namespace = var.vault.vault_namespace
+  dynamic auth_login {
+    for_each = (var.vault.vault_role_id  != "" && var.vault.vault_secret_id != "") ? [1] : []
+    content {
+      namespace  = var.vault.vault_namespace
+      path       = "auth/approle/login"
+      parameters = {
+        role_id   = var.vault.vault_role_id
+        secret_id = var.vault.vault_secret_id
+      }
+    }
+  }
+} 
+
+############################### DATA READ #######################################
+
+data "vault_generic_secret" "gcp_owner" {
+  path = var.path_vault_gcp_token
+}
+
+############################### RESOURCE ########################################
+
+
+resource "google_storage_bucket" "test_bucket" {
+for_each = var.bucketname
+ name          = each.value["name"]
+ location      = "EU"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = each.value["acces_level"]
+}
